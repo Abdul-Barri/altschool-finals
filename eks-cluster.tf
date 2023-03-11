f58b1fa7e075b45d232ac5458efc00c7cc0cd390
Abdul-Barri <lawalabdulbarri@gmail.com>
2023-03-11 01:47:58 +0100

    terraform files

---
+++ eks-cluster.tf
@@ -0,0 +1,47 @@
+# Create EKS Cluster
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "eks-cluster-portfolio"
+  role_arn = aws_iam_role.eks-cluster-role.arn
+  # cluster_endpoint_public_access = true
+
+  vpc_config {
+    security_group_ids = [aws_security_group.eks-security-group.id]
+    subnet_ids = [aws_subnet.pub-sub1.id, aws_subnet.pub-sub2.id, aws_subnet.pub-sub3.id, aws_subnet.priv-sub1.id, aws_subnet.priv-sub2.id, aws_subnet.priv-sub3.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSVPCResourceController,
+  ]
+}
+
+# Create EKS Cluster node group
+
+resource "aws_eks_node_group" "eks-node-group" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks-nodes-role.arn
+  instance_types = ["t2.micro"]
+  subnet_ids      = [aws_subnet.pub-sub1.id, aws_subnet.pub-sub2.id, aws_subnet.pub-sub3.id, aws_subnet.priv-sub1.id, aws_subnet.priv-sub2.id, aws_subnet.priv-sub3.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+# output "endpoint" {
+#   value = aws_eks_cluster.example.endpoint
+# }
+
+# output "kubeconfig-certificate-authority-data" {
+#   value = aws_eks_cluster.example.certificate_authority[0].data
+# }
---
+++ s3backend.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-08174509694"
+    key = "global/s3/terraform.tfstate"
+    region     = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.accesskey
+  secret_key = var.secretkey
+
+}
+
+# Create S3 Bucket for backend statefile
+
+resource "aws_s3_bucket" "terraform-state-bucket" {
+  bucket = var.backend-bucket-name
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Create Dynamo DB table for backend statefile locking
+
+resource "aws_dynamodb_table" "terraform-state-lock" {
+  name             = var.backend-lock-name
+  hash_key         = "LockID"
+  billing_mode     = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
