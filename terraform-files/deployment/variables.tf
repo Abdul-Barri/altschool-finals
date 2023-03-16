b4dfaa12cbdee3b9397d2a0265b7252bab16c83d
Abdul-Barri <lawalabdulbarri@gmail.com>
2023-03-16 21:53:27 +0100

    1st exec workflow

---
+++ terraform-files/deployment/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster" {
+  default = "eks-cluster"
+}
+
+variable "app" {
+  type        = string
+  description = "Name of application"
+  default     = "portfolio"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "docker-image" {
+  type        = string
+  description = "name of the docker image to deploy"
+  default     = "abdulbarri/portfolio:1.0"
+}
