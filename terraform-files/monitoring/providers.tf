f8567163ae480e398ad6c9655397b7a94d8350d4
Abdul-Barri <lawalabdulbarri@gmail.com>
2023-03-18 15:40:54 +0100

    monitoring pipeline#2

--- terraform-files/monitoring/providers.tf
+++ terraform-files/monitoring/providers.tf
@@ -59,18 +59,18 @@ provider "kubectl" {
 
 # Retrieve EKS cluster authentication token
 
-# data "aws_eks_cluster_auth" "eks-token" {
-#   name = "eks-cluster"
-# }
+data "aws_eks_cluster_auth" "eks-token" {
+  name = "eks-cluster"
+}
 
-# # Heml provider configuration
+# Heml provider configuration
 
-# provider "helm" {
-#   kubernetes {
-#   host                   = data.aws_eks_cluster.eks-cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-#   token = data.aws_eks_cluster_auth.eks-token.token
+provider "helm" {
+  kubernetes {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.eks-token.token
 
-#   }
+  }
  
-# }
+}
