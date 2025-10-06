7cb7084f03ce2afb9ac63fba3de06ce29405ece6
Abdul Barri Lawal <lawalabdulbarri@gmail.com>
2025-10-06 23:08:09 +0000

    test

--- HelloWorldFunc/HelloWorld.cs
+++ HelloWorldFunc/HelloWorld.cs
@@ -1,7 +1,7 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using Microsoft.Azure.Functions.Worker.Http;
 
 namespace HelloWorldFunc
 {
